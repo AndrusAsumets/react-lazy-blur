968df1e3dc2dfb88c649a1f89acf755f8e92a2ac
Andrus Asumets <andrus.asumets@gmail.com>
2017-07-25 23:18:13 +0000

    webpack

--- scripts/93cfdb40b3c093cd8efca24a98099fef.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Container from './components/container'
-import '../styles/app.css'
-
-render(
-    <Container />,
-    document.getElementById('container')
-)
