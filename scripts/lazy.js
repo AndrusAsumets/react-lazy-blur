e32c4c99147db98d5783f8aec59f2640a33319f4
Andrus Asumets <andrus.asumets@gmail.com>
2017-07-26 22:57:04 +0000

    update images

---
+++ scripts/lazy.js
@@ -0,0 +1,29 @@
+const folder = './src/images/'
+const fs = require('fs')
+const Jimp = require('jimp')
+const files = fs.readdirSync(folder)
+
+const process = async function(i) {
+	if(i < files.length ) {
+		const file = files[i]
+		const regex = /\.(jpe?g||png)$/
+		
+		if (
+			regex.test(file) == false &&
+			/\.(.blur)$/.test(file) == false
+		) return process(i + 1)
+		
+		console.log('Lazying:', folder + file)
+		const image = await Jimp.read(folder + file).catch((err) => { return console.log(err) })
+		image.resize(1280, Jimp.AUTO)
+		image.quality(90)
+		
+		const prefix = file.split(regex)[0]
+		const suffix = file.split(regex)[1]
+		const saveName = prefix + '.lazy.' + suffix
+		const save = await image.write(folder + saveName)
+		
+		process(i + 1)
+	}
+}
+process(0)
