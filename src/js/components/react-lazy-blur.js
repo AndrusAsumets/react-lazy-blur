c77b44b5ef341c934f16838833887ba2d61c8186
Andrus Asumets <andrus.asumets@gmail.com>
2017-07-26 05:31:27 +0300

    Update react-lazy-blur.js

--- src/js/components/react-lazy-blur.js
+++ src/js/components/react-lazy-blur.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, {
+    Component
+} from 'react'
 import Waypoint from 'react-waypoint'
 
 export default class ReactLazyBlur extends React.Component {
@@ -6,7 +8,7 @@ export default class ReactLazyBlur extends React.Component {
         super(props)
 
         this.state = {
-			waypoint: false,
+            waypoint: false,
             blur: {},
             background: {},
             loaded: false
@@ -17,52 +19,55 @@ export default class ReactLazyBlur extends React.Component {
         if (!this.props.background) return console.log('ThisLazy warning: currently too lazy to look for a background image. Please provide one.')
         else this.lazy()
     }
-    
+
     lazy() {
-    	const { background, blur } = this.props
-    	const duration = this.props.duration || 500
+        const {
+            background,
+            blur
+        } = this.props
+        const duration = this.props.duration || 500
         const self = this
-        
+
         //display blurred image on the place of background before load
         self.setState({
             loading: true,
             background: {
-            	backgroundImage: `url(${ blur })`
+                backgroundImage: `url(${ blur })`
             }
         })
-		
-		//start loading background image
+
+        //start loading background image
         const img = new Image()
         img.onload = () => {
-        	
-        	//on load, display the background and swap background with blur
+
+            //on load, display the background and swap background with blur
             self.setState({
-            	background: {
-            		backgroundImage: `url(${ img.src })`
-            	},
+                background: {
+                    backgroundImage: `url(${ img.src })`
+                },
                 blur: {
-                	backgroundImage: `url(${ blur })`,
+                    backgroundImage: `url(${ blur })`,
                     opacity: 1
                 }
             })
-			
-			//wait for next tick, so opacity from the previous block could be first added to the DOM
+
+            //wait for next tick, so opacity from the previous block could be first added to the DOM
             setTimeout(() => {
-            	
-            	//fade out the blurred image by changing the opacity
+
+                //fade out the blurred image by changing the opacity
                 self.setState({
                     background: {
-						backgroundImage: `url(${ img.src })`
+                        backgroundImage: `url(${ img.src })`
                     },
                     blur: {
-						backgroundImage: `url(${ blur })`,
+                        backgroundImage: `url(${ blur })`,
                         transition: 'opacity',
                         transitionDuration: duration / 1000 + 's',
                         opacity: 0
                     }
                 })
-				
-				//finally remove the blur and its instances
+
+                //finally remove the blur and its instances
                 setTimeout(() => {
                     self.setState({
                         loaded: true
@@ -74,37 +79,91 @@ export default class ReactLazyBlur extends React.Component {
     }
 
     render() {
-    	const { className, children } = this.props
-    	const { background, blur, waypoint } = this.state
-    	
-        return (
-        		<span>
-        	
-	        		<Waypoint onEnter={() => { this.setState({ waypoint: true }) }} scrollableAncestor={ window } />
-	        		
-	        		{
-	        			waypoint ?
-	        				<div className={ 'react-lazy-blur ' + className } style={{ position: 'relative' }}>
-			        			<span>
-					                <div className={ 'react-lazy-blur background' } style={ background }>
-					                    { children }
-					                </div>
-					
-					                {
-					                    !this.state.loaded ?
-					                        <div className={ 'react-lazy-blur blur' } style={ Object.assign({ position: 'absolute', top: 0 }, blur )}>
-					                            { children }
-					                        </div>
-					                    : null
-					                }
-			        			</span>
-		        			</div>
-        				: null
-	        		}
-		            
-	        		<Waypoint onEnter={() => { this.setState({ waypoint: true }) }} scrollableAncestor={ window } />
-        		
-        		</span>
+        const {
+            className,
+            children
+        } = this.props
+        const {
+            background,
+            blur,
+            waypoint
+        } = this.state
+
+        return ( <
+            span >
+
+            <
+            Waypoint onEnter = {
+                () => {
+                    this.setState({
+                        waypoint: true
+                    })
+                }
+            }
+            scrollableAncestor = {
+                window
+            }
+            />
+
+            {
+                waypoint ?
+                    <
+                    div className = {
+                        'react-lazy-blur ' + className
+                    }
+                style = {
+                        {
+                            position: 'relative'
+                        }
+                    } >
+                    <
+                    span >
+                    <
+                    div className = {
+                        'react-lazy-blur background'
+                    }
+                style = {
+                        background
+                    } > {
+                        children
+                    } <
+                    /div>
+
+                {
+                    !this.state.loaded ?
+                        <
+                        div className = {
+                            'react-lazy-blur blur'
+                        }
+                    style = {
+                            Object.assign({
+                                position: 'absolute',
+                                top: 0
+                            }, blur)
+                        } > {
+                            children
+                        } <
+                        /div>: null
+                } <
+                /span> <
+                /div>: null
+            }
+
+            <
+            Waypoint onEnter = {
+                () => {
+                    this.setState({
+                        waypoint: true
+                    })
+                }
+            }
+            scrollableAncestor = {
+                window
+            }
+            />
+
+            <
+            /span>
         )
     }
 }
